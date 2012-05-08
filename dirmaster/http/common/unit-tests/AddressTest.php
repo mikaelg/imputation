a4fb9e47c246eca5593edd3044a2b10bd727538d
Jos Bolssens (@ imac OWS) <marvelade@gmail.com>
2012-05-08 16:57:09 +0200

    Address class herwerkt met lege string en getal 0 initialisaties voor de properties. __get() method toegevoegd. unit test herwerkt hiernaar

--- dirmaster/http/common/unit-tests/AddressTest.php
+++ dirmaster/http/common/unit-tests/AddressTest.php
@@ -1,14 +1,32 @@
 <?php
   
-  require_once "c:\\xampp\\htdocs\\imputation\\dirmaster\\common\\classes\\iEntity.php";
-  require_once "c:\\xampp\\htdocs\\imputation\\dirmaster\\common\\classes\\Address.php";
+  require_once dirname(dirname(__FILE__))."/classes/iEntity.php";
+  require_once dirname(dirname(__FILE__))."/classes/Address.php";
   class AddressTest extends PHPUnit_Framework_TestCase
   {
 
-    public function testInputArrayWithGoodies()
+    public function testAddressConstructor()
     {
-      
-        $g1 = array(	"id"				=> 5,
+		$a1 = new Common\Address();
+		/*$flds = $a1 -> __get('fields');
+		foreach($flds as $fieldname => $fieldspecs_arr)
+		{
+			$fn = $a1 -> __get($fieldname);
+			$this->assertTrue(empty($fn));
+			$this->assertTrue(is_null($a1 -> __get($fieldname)));
+		}*/
+		
+		
+		
+		$this->assertTrue($a1 -> __get('id') !== false);
+		$this->assertTrue($a1 -> __get('country') !== false);
+		$this->assertFalse($a1 -> __get('fgdhjsdfgkjhsgdfkjhg') !== false);
+
+		
+    }
+    
+    
+    /*$g1 = array(	"id"				=> 5,
 							"country" 			=> 'BEL',
 							"city"				=> 'Gent',
 							"street"			=> 'Dok Noord',
@@ -18,7 +36,7 @@
 							"province" 			=> 'O-VL',
 							"organisationId" 	=> 4,
 							"addressTypeId"		=> 9);
-  			$g2 = array(	"id"				=> 5,
+  		$g2 = array(	"id"				=> 5,
 							"country" 			=> 'BEL',
 							"city"				=> 'Gent',
 							"street"			=> 'Dok Noord',
@@ -26,12 +44,10 @@
 							"nep-veld"			=> 'wtf stade gij hier te doen??',
 							"province" 			=> 'O-VL',
 							"organisationId" 	=> 4,
-							"addressTypeId"		=> 9);
-        $this->assertTrue(Common\Address::inputArrayIsValid($g1, true));
-        $this->assertTrue(Common\Address::inputArrayIsValid($g2, true));
-    }
+							"addressTypeId"		=> 9);*/
     
-     public function testInputArrayWithBaddies()
+    
+    /*public function testInputArrayWithBaddies()
     {
         $b1 = array(	"id"				=> 'stringinsteadofint',
 							"country" 			=> 'BEL',
@@ -56,7 +72,7 @@
   							);
         $this->assertFalse(Common\Address::inputArrayIsValid($b1, true));
         $this->assertFalse(Common\Address::inputArrayIsValid($b2, true));
-    }
+    }*/
     
  
           
