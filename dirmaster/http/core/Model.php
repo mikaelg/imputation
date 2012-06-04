6f6117aae821402e6af605e33e3d0b57611e0158
Mikael Gyselinck <mikael.gyselinck@gmail.com>
2012-06-04 21:57:05 +0200

    db pwd mig

--- dirmaster/http/core/Model.php
+++ dirmaster/http/core/Model.php
@@ -29,7 +29,7 @@ class Model{
 					
 			$dal = new \PDO( 'mysql:host=localhost;dbname=imputation',
 						'root',
-						'',
+						'root',
 						array(
 								\PDO::MYSQL_ATTR_USE_BUFFERED_QUERY    => 1,
 								\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
